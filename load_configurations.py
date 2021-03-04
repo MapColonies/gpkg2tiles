8169ee050046b595354b72efca834364f8b897e5
Anguiar <shacharroy14@gmail.com>
2021-03-04 09:50:34 +0200

    feat(storage): add fs support

--- load_configurations.py
+++ load_configurations.py
@@ -4,20 +4,23 @@ FILE_NAME = '.settings'
 config = configparser.ConfigParser()
 config.read(FILE_NAME)
 
+is_fs = False if config['STORAGE_PROVIDER']['FS'] == 'FALSE' else True
 aws_config = ""
-bucket = config['AWS']['bucket']
 
-if config['AWS']['SecretAccessKey']:
-    aws_config += "--config AWS_SECRET_ACCESS_KEY {0} ".format(config['AWS']['SecretAccessKey'])
+if not is_fs:
+    bucket = config['AWS']['bucket']
 
-if config['AWS']['AccessKeyId']:
-    aws_config += "--config AWS_ACCESS_KEY_ID {0} ".format(config['AWS']['AccessKeyId'])
+    if config['AWS']['SecretAccessKey']:
+        aws_config += "--config AWS_SECRET_ACCESS_KEY {0} ".format(config['AWS']['SecretAccessKey'])
 
-if config['AWS']['Endpoint']:
-    aws_config += "--config AWS_S3_ENDPOINT {0} ".format(config['AWS']['Endpoint'])
+    if config['AWS']['AccessKeyId']:
+        aws_config += "--config AWS_ACCESS_KEY_ID {0} ".format(config['AWS']['AccessKeyId'])
 
-if config['AWS']['Https']:
-    aws_config += "--config AWS_HTTPS {0} ".format(config['AWS']['Https'])
+    if config['AWS']['Endpoint']:
+        aws_config += "--config AWS_S3_ENDPOINT {0} ".format(config['AWS']['Endpoint'])
 
-if config['AWS']['VirtualHosting']:
-    aws_config += "--config AWS_VIRTUAL_HOSTING {0} ".format(config['AWS']['VirtualHosting'])
+    if config['AWS']['Https']:
+        aws_config += "--config AWS_HTTPS {0} ".format(config['AWS']['Https'])
+
+    if config['AWS']['VirtualHosting']:
+        aws_config += "--config AWS_VIRTUAL_HOSTING {0} ".format(config['AWS']['VirtualHosting'])
