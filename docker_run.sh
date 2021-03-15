fc93a3d0df0d4f90d5bcf78fa8f4702927bb09a6
shimoncohen <shimoncohen655@gmail.com>
2021-03-15 15:41:40 +0200

    Revert "chore: add print to indicate the start of the process"
    This reverts commit 05862c4b01d194ee10221329ea8da49d9c547fdb.

--- docker_run.sh
+++ docker_run.sh
@@ -1,13 +1,2 @@
-SETTINGS_FILE_LOCATION=""
-GPKG_FILE_LOCATION=""
-OUTPUT_DIRECTORY=""
-
-test -z $SETTINGS_FILE_LOCATION && echo "Please enter settings file location" && exit
-test -z $GPKG_FILE_LOCATION && echo "Please enter geopackage location" && exit
-test -z $OUTPUT_DIRECTORY && echo "Please enter output directory location" && exit
-
-docker run --rm --name gpkg2tiles -i \
-    -v $SETTINGS_FILE_LOCATION:/app/.settings \
-    -v $GPKG_FILE_LOCATION:/app/bluemar.gpkg \
-    -v $OUTPUT_DIRECTORY:/app/output \
-    gpkg2tiles
+docker run --rm --name gpkg2tiles -it \
+gpkg2tiles
