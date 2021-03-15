d8663c47f3cacece3adbd7ab7c40572f283a14d2
shimoncohen <shimoncohen655@gmail.com>
2021-03-15 16:48:52 +0200

    chore: remove debugging entrypoint

--- tile_script/docker_run.sh
+++ tile_script/docker_run.sh
@@ -1,11 +1,10 @@
 GPKG_FILE_LOCATION=""
 OUTPUT_DIRECTORY=""
 
-test -z $SETTINGS_FILE_LOCATION && echo "Please enter settings file location" && exit
 test -z $GPKG_FILE_LOCATION && echo "Please enter geopackage location" && exit
 test -z $OUTPUT_DIRECTORY && echo "Please enter output directory location" && exit
 
-docker run --rm --name gpkg2tilesscript -it --entrypoint /bin/bash \
+docker run --rm --name gpkg2tilesscript \
     -v $GPKG_FILE_LOCATION:/app/bluemar.gpkg \
     -v $OUTPUT_DIRECTORY:/app/output \
     -e OUTPUT_DIRECTORY=/app/output \
